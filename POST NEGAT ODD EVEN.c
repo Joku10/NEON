2d888446a87c637729bcabdd2cdb5ac89607482b
Lijo Jolly <lijojolly10@gmail.com>
2024-03-22 00:21:36 +0530

    Add files via upload

---
+++ POST NEGAT ODD EVEN.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void main()
+{
+    int num;
+    printf("\nEnter a Number: ");
+    scanf("%d",&num);
+
+    if (num>0)
+    {
+        printf("The number is postive");
+    }
+    
+    else if(num<0)
+    {
+        printf("The number is positive");
+    }
+    
+   if (num>0)
+   {
+    if(num%2==0)
+    
+        printf("The Number is even");
+    else if
+    
+        printf("The Number is odd");
+    
+   }
+}
